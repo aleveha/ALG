934a166a9ae0c36c633914a9ec6f036cdb268fb2
aleveha <nikita.tashilin@gmail.com>
2021-03-17 14:55:09 +0100

    edit Check_Git_App

--- 00_Check_Git/src/Check_Git_App.java
+++ 00_Check_Git/src/Check_Git_App.java
@@ -1,5 +1,7 @@
 public class Check_Git_App {
     public static void main(String[] args) {
         System.out.println("Hello ALG!");
+        System.out.println("Hello ALG!");
+        System.out.println("Hello ALG!");
     }
 }
