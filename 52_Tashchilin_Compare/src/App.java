ab7cba8d0671114b8727a86ac0046810c79584b6
aleveha <nikita.tashilin@gmail.com>
2021-04-14 14:24:41 +0200

    add comments

--- 52_Tashchilin_Compare/src/App.java
+++ 52_Tashchilin_Compare/src/App.java
@@ -4,19 +4,23 @@ import java.util.Comparator;
 import java.util.List;
 
 public class App {
+    // data
     private static final List<Student> students = new ArrayList<>();
 
     public static void main(String[] args) {
+        // set students
         students.add(new Student(1, "Mike", "Fanek", 2.5));
         students.add(new Student(3, "Tom", "Lord", 2.0));
         students.add(new Student(2, "Alice", "Acile", 1.5));
         students.add(new Student(4, "Bob", "Herany", 3.6));
 
+        // create different sorting methods
         Comparator<Student> sortById = Comparator.comparing(Student::getId);
         Comparator<Student> sortByFirstName = Comparator.comparing(Student::getFirstName);
         Comparator<Student> sortByLastName = Comparator.comparing(Student::getLastName);
         Comparator<Student> sortByAverage = Comparator.comparing(Student::getAverage);
 
+        // print sorted arrays
         printSortedList("Not sorted", (o1, o2) -> 0);
         printSortedList("Sorted by id", sortById);
         printSortedList("Sorted by first name", sortByFirstName);
