12760d4515048b7d1d8f07801d0eb83318e4a2c4
aleveha <nikita.tashilin@gmail.com>
2021-04-14 14:21:51 +0200

    fix small errors

--- 52_Tashchilin_Compare/src/App.java
+++ 52_Tashchilin_Compare/src/App.java
@@ -4,8 +4,9 @@ import java.util.Comparator;
 import java.util.List;
 
 public class App {
+    private static final List<Student> students = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<Student> students = new ArrayList<>();
         students.add(new Student(1, "Mike", "Fanek", 2.5));
         students.add(new Student(3, "Tom", "Lord", 2.0));
         students.add(new Student(2, "Alice", "Acile", 1.5));
@@ -16,14 +17,14 @@ public class App {
         Comparator<Student> sortByLastName = Comparator.comparing(Student::getLastName);
         Comparator<Student> sortByAverage = Comparator.comparing(Student::getAverage);
 
-        UI(students, "Not sorted", (o1, o2) -> 0);
-        UI(students,"Sorted by id", sortById);
-        UI(students,"Sorted by first name", sortByFirstName);
-        UI(students,"Sorted by last name", sortByLastName);
-        UI(students,"Sorted by average", sortByAverage);
+        printSortedList("Not sorted", (o1, o2) -> 0);
+        printSortedList("Sorted by id", sortById);
+        printSortedList("Sorted by first name", sortByFirstName);
+        printSortedList("Sorted by last name", sortByLastName);
+        printSortedList("Sorted by average", sortByAverage);
     }
 
-    private static void UI(List<Student> students, String label, Comparator<Student> sortBySomething) {
+    private static void printSortedList(String label, Comparator<Student> sortBySomething) {
         System.out.println(Colors.BG_GREEN + Colors.BLACK + label + Colors.RESET_COLOR);
         students.sort(sortBySomething);
         students.forEach(System.out::println);
